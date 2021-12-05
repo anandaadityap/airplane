f18a50beb39c82574f6f232ccb5676701cf901ee
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-05 09:24:26 +0700

    Slicing Home => bagian popular destination

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -54,8 +54,84 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestinations() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                left: defaultMargin,
+              ),
+              padding: EdgeInsets.all(10),
+              width: 200,
+              height: 323,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: kWhiteColor,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: 180,
+                    height: 220,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      image: DecorationImage(
+                        image: AssetImage('assets/image_destination1.png'),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        height: 30,
+                        width: 55,
+                        decoration: BoxDecoration(
+                          color: kWhiteColor,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(18),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 18,
+                              height: 18,
+                              margin: EdgeInsets.only(right: 2),
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/icon_Star.png'),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '4.8',
+                              style: blackTextStyle.copyWith(
+                                fontWeight: medium,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return ListView(
-      children: [header()],
+      children: [
+        header(),
+        popularDestinations(),
+      ],
     );
   }
 }
