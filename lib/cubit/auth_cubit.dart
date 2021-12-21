293b35e2685032910e665605f2eb02d391e44a16
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-21 07:31:52 +0700

    Menambahkan Auth State dan User Model

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:airplane/models/user_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String hobby;
+  final int balance;
+
+  UserModel(
+      {required this.id,
+      required this.email,
+      this.name = '',
+      this.hobby = '',
+      this.balance = 0});
+
+  @override
+  List<Object?> get props => [id, email, name, hobby, balance];
+}
