30ee981410e86f033c61923b747afee0206608a4
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-21 08:01:23 +0700

    menambahkan sign up auth service & user service

---
+++ lib/services/user_service.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+import 'package:airplane/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserService {
+  CollectionReference _userReference =
+      FirebaseFirestore.instance.collection('users');
+
+  Future<void> setUSer(UserModel user) async {
+    try {
+      _userReference.doc(user.id).set({
+        'email': user.email,
+        'name': user.name,
+        'hobby': user.hobby,
+        'balance': user.balance
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+}
