44bd0df2c7ae726c26a0187fbea8ace310fdbc46
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-26 09:58:35 +0700

    menambah destination Model & state

---
+++ lib/cubit/destination_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:airplane/models/destination_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'destination_state.dart';
+
+class DestinationCubit extends Cubit<DestinationState> {
+  DestinationCubit() : super(DestinationInitial());
+}
