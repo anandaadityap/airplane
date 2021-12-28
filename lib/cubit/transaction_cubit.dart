7b2acee77a8c9498b41df7cbb2db8e772f860966
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-28 08:31:11 +0700

    menambahkan method fetch transactions

---
+++ lib/cubit/transaction_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:airplane/models/transaction_model.dart';
+import 'package:airplane/services/transaction_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'transaction_state.dart';
+
+class TransactionCubit extends Cubit<TransactionState> {
+  TransactionCubit() : super(TransactionInitial());
+
+  void createTransaction(TransactionModel transaction) async {
+    try {
+      emit(TransactionLoading());
+
+      await TransactionService().createTransaction(transaction);
+
+      emit(TransactionSuccess([]));
+    } catch (e) {
+      emit(TransactionFailed(e.toString()));
+    }
+  }
+
+  void fetchTransactions() async {
+    try {
+      emit(TransactionLoading());
+
+      List<TransactionModel> transations =
+          await TransactionService().fetchTransactions();
+
+      emit(TransactionSuccess(transations));
+    } catch (e) {
+      emit(TransactionFailed(e.toString()));
+    }
+  }
+}
