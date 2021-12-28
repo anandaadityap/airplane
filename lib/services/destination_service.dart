66dfd277b4c43f385e6da18d856d14186e104888
anandaadt11 <anandaadityaputra11@gmail.com>
2021-12-28 08:29:42 +0700

    menambahkan method fetch transactions

--- lib/services/destination_service.dart
+++ lib/services/destination_service.dart
@@ -2,12 +2,12 @@ import 'package:airplane/models/destination_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DestinationService {
-  CollectionReference _destinationRef =
+  CollectionReference _destinationReference =
       FirebaseFirestore.instance.collection('destinations');
 
   Future<List<DestinationModel>> fetchDestinations() async {
     try {
-      QuerySnapshot result = await _destinationRef.get();
+      QuerySnapshot result = await _destinationReference.get();
 
       List<DestinationModel> destination = result.docs.map(
         (e) {
